40b839374d7d2f2b190a67dc90c9bf4e37486ead
louisdcerda <93852998+louisdcerda@users.noreply.github.com>
2022-04-20 18:52:23 -0700

    Add files via upload

--- calc.cpp
+++ calc.cpp
@@ -10,41 +10,65 @@ using namespace std;
 
 Calc::Calc(char* argvIn)
 {
+    //initialize variables
+    InitializeVars();
+    //check for valid input
+    if(!CheckTokens())
+    {
+        cout << "Invalid input.  Terminating program." << endl;
+        exit(EXIT_FAILURE);
+    }
+    //create value table
+    MakeValueTbl();
+    //parse input
+    Parse();
+    //check for balanced parentheses
+    if(!CheckParens())
+    {
+        cout << "Unbalanced parentheses.  Terminating program." << endl;
+        exit(EXIT_FAILURE);
+    }
 }
 
 Calc::~Calc()
 {
+    //delete dynamically allocated memory
+    delete [] valueTbl;
+    delete [] infix;
+    delete [] postfix;
 }
 
 bool Calc::CheckTokens()
 {
-    string mathOps = argv[1];
     int length = 0;
-    for( int i = 0; i < mathOps.len(); i ++)
+    for( int i = 0; i < argv[1].length(); i ++)
     {
-        if ( mathOps[i] == '(' or mathOps[i] == ')' )
+        if ( argv[1][i] == '(' or argv[1][i] == ')' )
             length++;
-        else if (mathOps[i].isalpha() and mathOps[i].isupper())
+        else if (argv[1].isalpha() and argv[1].isupper())
             length++;
-        else if (mathOps[i] == '+' or mathOps[i] == '-')
+        else if (argv[1][i] == '+' or argv[1][i] == '-')
             length++;
-        else if (mathOps[i] == '*' or mathOps[i] == '/')
+        else if (argv[1][i] == '*' or argv[1][i] == '/')
             length ++;
         for ( int k = 0; k < 10; i++)
         {
-            if( mathOps[i] == k )
+            if( argv[1][i] == k )
                 length ++;
         }
     }
 
-    if (length == mathOps.len())
+    if (length == argv[1].length())
         return true;
     return false;
 }
 
 void Calc::MakeValueTbl()
 {
-
+    char* table = new char[26];
+    valueIdx = 0;
+    for (int i = 0; i < 26; i++)
+        table[i] = 0;
 }
 
 void Calc::Parse()
@@ -54,8 +78,21 @@ void Calc::Parse()
 
 bool Calc::CheckParens()
 {
- return true;
+    int left = 0;
+    int right = 0;
+    for ( int i = 0; i < argv[1].length(); i++)
+    {
+        if ( argv[1][i] == '(')
+            left++;
+        else if ( argv[1][i] == ')')
+            right++;
+    }
+    if ( left == right)
+        return true;
+ return false;
 }
 
 void Calc::DisplayInFix()
-{}
+{
+
+}
