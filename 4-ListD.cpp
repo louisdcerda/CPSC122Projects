f35ae1f7e657873945fcd33cfd6f8f50ee89a14c
louisdcerda <93852998+louisdcerda@users.noreply.github.com>
2022-04-20 21:36:30 -0700

    Add files via upload

--- 4-ListD.cpp
+++ 4-ListD.cpp
@@ -10,24 +10,24 @@ ListD<T>::ListD()
  InitializeVars();
 }
 
-template<typename T>
+template <typename T>
 void ListD<T>::Insert(T item, int pos)
 {
- //new node goes between these two nodes
- doubleNode<T>* insertPtA = FindPosition(pos);  
- doubleNode<T>* insertPtB = insertPtA->next; 
+  //new node goes between these two nodes
+  doubleNode<T>* insertPtA = FindPosition(pos);  
+  doubleNode<T>* insertPtB = insertPtA->next; 
 
- //create new node and set its values
- doubleNode<T>* tmp = new doubleNode<T>*; 
- tmp->prev = insertPtA;
- tmp->item = item;
- tmp->next = insertPtB;
+  //create new node and set its values
+  doubleNode<T>* tmp = new doubleNode<T>; 
+  tmp->prev = insertPtA;
+  tmp->item = item;
+  tmp->next = insertPtB;
 
- //set pointers for nodes before and after the insertion point
- insertPtA->next = tmp;
- insertPtB->prev = tmp;
+  //set pointers for nodes before and after the insertion point
+  insertPtA->next = tmp;
+  insertPtB->prev = tmp;
 
- length++;
+  length++;
 }
 
 template<typename T>
@@ -41,6 +41,9 @@ void ListD<T>::Delete(int pos)
  deletePtNext->prev = deletePtPrev;
 
  delete deletePt;
+ deletePt = NULL;
+ deletePtNext = NULL;
+  deletePtPrev = NULL;
  length--;
 }
 
@@ -49,8 +52,8 @@ void ListD<T>::PrintForward()
 {
  doubleNode<T>*  cur = head->next;
 
- int i = 0;
- while (i < length)
+ int i = 1;
+ while (i < length + 1)
  {
   cout << cur->item << endl;
   cur = cur->next;
@@ -92,19 +95,20 @@ void ListD<T>::InitializeVars()
  tail->next = NULL;
 }
 
-template<typename T>
+template <typename T>
 doubleNode<T>* ListD<T>::FindPosition(int pos)
 {
-    doubleNode<T>* temp = head;
-    int pass = 0;
-
-    while (temp->data != pos && temp->next != NULL) 
-    {
-        pos++;
-        temp = temp->next;
-    }
-    return (pos + 1);
-}
+  //Inserting at the tail is a special case.  It can be made much more efficient than
+  //this.
+  doubleNode<T>* cur = head;
+  int i = 0;  //begin at the dummy node
+  while (i < pos - 1)
+  {
+    cur = cur->next;
+    i++;
+  }
+  return cur;
+} 
 
 template<typename T>
 ListD<T>::~ListD()
@@ -126,8 +130,96 @@ ListD<T>::ListD(const ListD<T>* lst)
  doubleNode<T>* cur = lst->FindPosition(2);
  for (int i = 1; i <= lst->length; i++)
  {
-  cout << cur->item << endl;
+  //cout << cur->item << endl;
   Insert(cur->item,i);
   cur = cur->next;
  }
+}
+
+template<typename T>
+void ListD<T>::Sort()
+{   
+  //sort the list using insertion sort
+  //start at the second node
+  doubleNode<T>* cur = head->next;
+  while (cur != tail)
+  {
+    //insert cur->item into the sorted list
+    doubleNode<T>* insertPt = head;
+    while (insertPt->next->item < cur->item)
+    {
+      insertPt = insertPt->next;
+    }
+    //insert cur->item into the sorted list
+    doubleNode<T>* tmp = cur->next;
+    cur->next = insertPt->next;
+    insertPt->next->prev = cur;
+    insertPt->next = cur;
+    cur->prev = insertPt;
+    cur = tmp;
+  } 
+}
+  // doubleNode<T>* cur = head->next;
+  // doubleNode<T>* min;
+  // doubleNode<T>* minPrev;
+  // doubleNode<T>* minNext;
+  // doubleNode<T>* temp;
+  // doubleNode<T>* tempPrev;
+  // doubleNode<T>* tempNext;
+  // int i = 0;
+  // while (i < length)
+  // {
+  //   min = cur;
+  //   minPrev = cur->prev;
+  //   minNext = cur->next;
+  //   cur = cur->next;
+  //   i++;
+  //   while (cur != tail)
+  //   {
+  //     if (cur->item < min->item)
+  //     {
+  //       min = cur;
+  //       minPrev = cur->prev;
+  //       minNext = cur->next;
+  //     }
+  //     cur = cur->next;
+//       }
+//       temp = min;
+//       tempPrev = minPrev;
+//       tempNext = minNext;
+//       minPrev->next = minNext;
+//       minNext->prev = minPrev;
+//       tempPrev->next = temp;
+//       temp->prev = tempPrev;
+//       temp->next = tempNext;
+//       tempNext->prev = temp;
+//       cur = temp;
+//     }
+// }
+
+template<typename T>
+int ListD<T>::DeleteAll(T item)
+{
+
+  int count = 0;
+  doubleNode<T>* cur = head;
+  doubleNode<T>* next;
+  doubleNode<T>* prev;
+
+  for(int i = 0; i < length; i++)
+  {
+    next = cur->next;
+    prev = cur->prev; 
+
+    if(cur->item == item)
+    {
+      count++;
+      prev->next = cur->next;
+      next->prev = cur->prev;
+      delete cur;
+      length--;
+    } 
+    cur = next;    
+  }
+  return count;
 }
--- 4-ListDTst.cpp
+++ 4-ListDTst.cpp
@@ -3,135 +3,133 @@
 using namespace std;
 
 #include "4-ListD.h"
+#include "4-ListD.cpp"
+
+// void TestInsert();
+// void TestCopyConstructor();
+// void TestDeleteAll(int item);
+// void TestPrintForward();
+// void TestPrintBackward();
+void TestSort();
+// void TestDelete();
 
 //All tests are separated by function
-void TestInsert();
-void TestCopyConstructor();
 
 int main()
 {
- TestInsert();
- //TestCopyConstructor();
-//  PrintBackwards();
-//  deleteall();
-//  sortysort();
+//  TestInsert();
+//  TestCopyConstructor();
+//  TestPrintBackward();
+//  TestDeleteAll(3);
+ TestSort();
+
+ return 0;
 }
 
 
 
-void TestInsert()
-{
+// void TestInsert()
+// {
+//  ListD<int>* lst = new ListD<int>;
+//  for (int i = 1; i <= 5; i++)
+//   lst->Insert(i, i);
 
- ListD<int>* lst = new ListD<int>;
- for (int i = 1; i <= 5; i++)
-  lst->Insert(i,i);
- lst->PrintForward();
+//  lst->PrintForward();
 
- //test general case insert
- cout << "Passed Insert Test 1 if 1 through 5 appear on subsequent lines" << endl;
- lst->PrintForward();
- cout << endl;
+//  //test general case insert
+//  cout << "Passed Insert Test 1 if 1 through 5 appear on subsequent lines" << endl;
+//  lst->PrintForward();
+//  cout << endl;
 
- //test insert at the head
- lst->Insert(0,1);
- cout << "Passed Insert Test 2 if 0 appears in position 1" << endl; 
- lst->PrintForward();
- cout << endl;
+//  //test insert at the head
+//  lst->Insert(0,1);
+//  cout << "Passed Insert Test 2 if 0 appears in position 1" << endl; 
+//  lst->PrintForward();
+//  cout << endl;
 
- //test insert at the tail 
- lst->Insert(100,7);
- cout << "Passed Insert Test 3 if 100 appears in final position" << endl; 
- lst->PrintForward();
- cout << endl;
+//  //test insert at the tail 
+//  lst->Insert(100,7);
+//  cout << "Passed Insert Test 3 if 100 appears in final position" << endl; 
+//  lst->PrintForward();
+//  cout << endl;
  
- //test insert in middle 
- lst->Insert(50,5);
- cout << "Passed Insert Test 4 if 50 appears in middle position" << endl; 
- lst->PrintForward();
- cout << endl;
- delete lst;
-}
+//  //test insert in middle 
+//  lst->Insert(50,5);
+//  cout << "Passed Insert Test 4 if 50 appears in middle position" << endl; 
+//  lst->PrintForward();
+//  cout << endl;
+//  delete lst;
+// }
+
 
 // void TestCopyConstructor()
 // {
-//  ListD<int>* lst1 = new ListD<int>;
-//  for (int i = 1; i <= 3; i++)
-//   lst1->Insert(i,i);
+//   ListD<int>* lst1 = new ListD<int>;
+//   for (int i = 1; i <= 3; i++)
+//    lst1->Insert(i,i);
 
-//  ListD<int>* lst2(lst1);
+//   ListD<int>* lst2(lst1);
  
-//  cout << "Traverse lst1" << endl;
-//  lst1->PrintForward();
-//  cout << endl;
-//  cout << "Traverse lst2" << endl;
-//  lst2->PrintForward();
+//   cout << "Traverse lst1" << endl;
+//   lst1->PrintForward();
+//   cout << endl;
+//   cout << "Traverse lst2" << endl;
+//   lst2->PrintForward();
 
-//  //delete lst1;
-//  //delete lst2;
-// }
+//   delete lst1;
+//   delete lst2;
+//  }
 
 
 
-// void PrintBackwards()
-// {
-//  ListD<int>* lst1 = new ListD<int>;
-//  for (int i = 1; i <= 3; i++)
-//   lst1->Insert(i,i);
+//  void TestPrintBackward()
+//  {
+//   ListD<int>* lst1 = new ListD<int>;
+//   for (int i = 1; i <= 3; i++)
+//    lst1->Insert(i,i);
  
-//  cout << "Traverse lst1 backwards" << endl;
-//  lst1->PrintBackward();
-//  cout << endl;
+//   cout << "Traverse lst1 backwards" << endl;
+//   lst1->PrintBackward();
+//   cout << endl;
 
-//  delete lst1;
-// }
-
-
-
-// void deleteall()
-// {
-//  ListD<int>* lst = new ListD<int>;
-//  for (int i = 1; i <= 7; i++)
-//   lst->Insert(i,i);
-// lst->PrintForward();
-
-//  cout << "All nodes containing instances of 3 have been deleted" << endl;
-//  lst->DeleteAll(3);
-//  cout << endl;
-// lst->PrintForward();
-//  cout << endl;
-//  cout << "All nodes containing instances of 4 have been deleted" << endl;
-//  lst->DeleteAll(4);
-// lst->PrintForward();
-
-//  delete lst1;
-// }
+//   delete lst1;
+//  }
 
 
 
-// void sortysort()
-// {
-//  ListD<int>* lst1 = new ListD<int>;
-//  lst1->Insert(100,1);
-//  lst1->Insert(27,2);
-//  lst1->Insert(98,3);
-//  lst1->Insert(99,4);
-//  lst1->Insert(1,5);
-//  lsst1->PrintForward();
-//  cout << "All nodes containing instances of 3 have been deleted" << endl;
-//  lst1->Sort();
-//  cout << endl;
+//  void TestDeleteAll(int item)
+//  {
+//   ListD<int>* lst = new ListD<int>;
+//   for (int i = 1; i <= 7; i++)
+//    lst->Insert(i,i);
 //  lst->PrintForward();
-//  delete lst1;
-// }
+
+//   cout << "All nodes containing instances of 3 have been deleted" << endl;
+//   cout << lst->DeleteAll(3);
+//   cout << endl;
+//   lst->PrintForward();
+//   cout << endl;
+//   cout << "All nodes containing instances of 4 have been deleted" << endl;
+//   lst->DeleteAll(4);
+//  lst->PrintForward();
+
+//   delete lst;
+//  }
 
 
 
-
-
-
-
-//  Write PrintBackward.  Compile/test your work. // done
-// 2. Write Delete.  Compile/test your work.
-// 3. Write the Destructor.  Compile/test your work.
-// 4. Write DeleteAll.  Compile/test your work.
-// 5. Write Sort. Compile/test your work.
+ void TestSort()
+ {
+  ListD<int>* lst1 = new ListD<int>;
+  lst1->Insert(100,1);
+  lst1->Insert(27,2);
+  lst1->Insert(98,3);
+  lst1->Insert(99,4);
+  lst1->Insert(1,5);
+  lst1->PrintForward();
+  cout << "The list has been sorted" << endl;
+  lst1->Sort();
+  cout << endl;
+  lst1->PrintForward();
+  // delete lst1;
+ }
